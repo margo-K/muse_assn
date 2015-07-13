a29f9d33f55748f28f28a4d221277ef5e7671927
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2015-07-13 17:54:13 +0200

    Working version! Removes unecessary dictionaries/passes through the lexicon

--- trending.py
+++ trending.py
@@ -6,74 +6,77 @@ import string
 import numpy as np
 import itertools
 import sys
+import datetime
 
 from fetcher import fetch_listings
 from helpers import get_list_items, get_date, trunc_date
 
-def _tokenize_listing(listing,time_period,stopwords=nltk.corpus.stopwords.words('English') + list(string.punctuation)):#need to add category, time_period
+def _parse_listing(listing,bigrams=True,trigrams=False,stopwords=None):
 	"""Returns a set of tokens in the listing, along with the listing's category and date"""
-	date = trunc_date(get_date(listing['creation_date']),time_period)
-
-	category = listing['categories']#Note: takes each category as a list.
-	if type(category) !=list:
-		raise CategoryError("Category Error: Cannot index {}".format(listing['id'])) 
-	category = tuple(category)
-
+	if not stopwords:
+		stopwords = nltk.corpus.stopwords.words('English') + list(string.punctuation)
+	category = listing['categories']#Note: takes each category as a list.	
 	description = listing['full_description']
 	qualities = get_list_items(description)
-	if not qualities:
-		return set(),[],""#this is in the case where there's no data to index
+	if not qualities or not category:
+		return set(),[],"" 
 
 	text = ". ".join([quality for quality in qualities])
-	tokens = [unigram.lower().strip(string.punctuation) for unigram in nltk.word_tokenize(text) if unigram not in stopwords and unigram != ""]#[item.lower().strip(string.punctuation) for item in text.split(' ') if item not in nltk.corpus.stopwords.words('English')+list(string.punctuation)]
-	if '' in tokens:
-		tokens.remove('')
-	bigrams = set(nltk.bigrams(tokens))
-	trigrams = set(nltk.trigrams(tokens))
-	return set(tokens).union(bigrams).union(trigrams), category, date
+	tokens = [token.lower().strip(string.punctuation) for token in text.split(' ') if token not in stopwords] 
+	all_tokens = set(tokens)
+	all_tokens.discard('')
+	if bigrams:
+		all_tokens |= set(nltk.bigrams(tokens))
+	if trigrams:
+		all_tokens |= set(nltk.trigrams(tokens))
+	return all_tokens, category , listing['creation_date']
 
-def get_lexicon(listings,time_period):
-	lexicon = set()
-	groups = {}
-	un_indexed = 0
+def get_lexicon(listings):
+	word_counter, un_indexed = 0, 0
+	lexicon_index = {}
+	lexicon, parsed_listings = [], []
+	
+	print "Building lexicon ..."
 	for listing in listings:
-		tokens, category, date = _tokenize_listing(listing,time_period)
-		if not tokens:
-			un_indexed +=1
+		tokens, categories, date = _parse_listing(listing)
+		if tokens:
+			int_tokens = []
+			for token in tokens:
+				token_index = lexicon_index.get(token)
+				if not token_index:
+					token_index = lexicon_index.setdefault(token,word_counter)
+					word_counter +=1
+					lexicon.append(token)
+				int_tokens.append(token_index)
+			parsed_listings.append((int_tokens,categories,date))
 		else:
-			lexicon |=tokens
-			group_listings = groups.setdefault(category,{})
-			category_date_listings = groups[category].setdefault(date,[])
-			try:
-				groups[category][date].append(tokens)
-			except:
-				pdb.set_trace()
-	print "Total Categories {}".format(len(groups))
-	total_words = len(lexicon)
-	lexicon_indexes = {word: index for index, word in enumerate(lexicon)}
-	size = len(lexicon_indexes)
+			un_indexed +=1
 
-	for category, date_groups in groups.iteritems():
-		for date, listings in date_groups.iteritems():
-			groups[category][date] = [_get_vectorized_listing(listing,lexicon_indexes,size) for listing in listings]
 	print "Total not indexed {}".format(un_indexed)
 	print "Portion not indexed: {}".format(un_indexed/len(listings))
-	return lexicon_indexes, groups #could be the OO values
+	return lexicon, parsed_listings
 
-def _get_vectorized_listing(listing_tokens,lexicon_indexes,lexicon_size):
-	"""Takes a set of tokens for a listing, a lexicon and the lexicon_size and returns
+def _group_listings(parsed_listings,lexicon_size,time_period):
+	groups = {}
+	for (int_tokens, categories, date) in parsed_listings:
+		date_period = trunc_date(get_date(date),time_period)
+		vector_listing = _get_vectorized_listing(int_tokens,lexicon_size)
+		for category in categories:
+			group_listings = groups.setdefault(category,{})
+			category_date_listings = groups[category].setdefault(date,[])
+			groups[category][date].append(vector_listing)
+	return groups
+
+def _get_vectorized_listing(int_tokens,lexicon_size):
+	"""Takes a list of word indexes for a listing and a lexicon_size and returns
 	the vector representation of the numpy array, where each element is a word
 	in the lexicon and each value is 0 or 1, depending on whether the word appears in the listing """
+	l = np.zeros(lexicon_size)
+	for int_token in int_tokens:
+		l[int_token] +=1
+	return l
 
-	v_listing = np.zeros(lexicon_size)
-	for token in listing_tokens:
-		index = lexicon_indexes[token]
-		v_listing[index]+=1
-	if lexicon_size !=len(lexicon_indexes):
-		pdb.set_trace
-	return v_listing
-
-def get_probabilities(listings_group):
+def _get_probabilities(listings_group):
 	"""Given a matrix of document-word vectors for a time period,
 	return a probability vector for which each element is the probability of 
 	seeing that word in that time period
@@ -110,7 +113,7 @@ def biggest_change(sorted_group_probabilities,limit):
  		old_average = np.zeros(len(sorted_group_probabilities[-1]))
  	deltas = sorted_group_probabilities[-1]-old_average
  	sorted_indices = np.argsort(deltas)
- 	trending = itertools.takewhile(lambda x: deltas[x]>0,sorted_indices)
+ 	trending = itertools.takewhile(lambda x: deltas[x]>0,sorted_indices[::-1])
 	top_trending = itertools.islice(trending,0,limit)
 	return top_trending
 
@@ -123,7 +126,7 @@ def get_trending(category_listings,trending_function=biggest_change,limit=10):
 
 		 =>  generator([0]), b/c the first element is the only one with a positive change """
 	
-	group_probabilities = [get_probabilities(category_listings[time_period]) for time_period in sorted(category_listings.keys())]
+	group_probabilities = [_get_probabilities(category_listings[time_period]) for time_period in sorted(category_listings.keys())]
 	return trending_function(group_probabilities,limit)
 
 def main(data_file='data/listings.json',time_period='year'):
@@ -133,16 +136,17 @@ def main(data_file='data/listings.json',time_period='year'):
 	else:
 		listings = fetch_listings()
 
-	lexicon_indexes, groups = get_lexicon(listings,time_period=time_period)
+	lexicon, parsed_listings = get_lexicon(listings)
+	groups = _group_listings(parsed_listings,len(lexicon),time_period)
+	print "Trending Items {} to {}".format(time_period,time_period)
 	for category, category_listings in groups.iteritems():
 		trending = get_trending(category_listings)
-		print "Category {} Trends\n".format(category)
-		for index in trending:
-			for word, word_index in lexicon_indexes.iteritems():
-				if index == word_index:
-					print word
-		# for word_or_phrase in trending:
-		# 	print ' '.join(word_or_phrase)
+		print "\n{}\n===========".format(category)
+		for rank, index in enumerate(trending):
+			word_or_phrase = lexicon[index]
+			if type(word_or_phrase) == tuple:
+				word_or_phrase = " ".join(word_or_phrase)
+			print "{}. {}".format(rank+1,word_or_phrase.encode('utf-8'))
 
 if __name__ == '__main__':
 	main()
